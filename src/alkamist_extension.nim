4423d5d159aee80d72e5d7e52389b02b40849e09
Corey Lehman <coreylehman@me.com>
2021-07-17 13:34:53 -0400

    Moved some things out of the reaper extension template.

--- src/alkamist_extension.nim
+++ src/alkamist_extension.nim
@@ -1,23 +1,8 @@
 import alkamist_extension/reaper
 
-# proc windowProc(hWnd: HWND, msg: UINT, wParam: WPARAM, lParam: LPARAM): INT_PTR {.stdcall.} =
-#   case msg:
-#   of WM_MOUSEMOVE:
-#     case LOWORD(wParam):
-#     of MK_LBUTTON:
-#       ShowConsoleMsg($GET_X_LPARAM(lParam))
-#     else:
-#       discard
-#   else:
-#     discard
-
-# let testWindow = CreateDialog(hInstance, MAKEINTRESOURCE(100), nil, windowProc)
-# if testWindow != nil:
-#   discard ShowWindow(testWindow, SW_SHOW)
-
 proc testActionFn() =
   ShowConsoleMsg("Test Action executed.\n")
 
-createReaperExtension:
+createExtension:
   addAction("Alkamist: Test Action", "ALKAMIST_TEST_ACTION", testActionFn)
   ShowConsoleMsg("Alkamist Extension initialized.\n")
--- src/alkamist_extension/reaper.nim
+++ src/alkamist_extension/reaper.nim
@@ -6,33 +6,49 @@ export
   tables,
   winapi, types, functions
 
-template createReaperExtension*(initCode: untyped): untyped =
+var
+  hInstance*: HINSTANCE
+  pluginInfo*: ptr reaper_plugin_info_t
+  actionProcs* = initTable[int, proc()]()
+
+proc hookCommand(command: cint, flag: cint): bool =
+  if command != 0 and actionProcs.contains(command):
+    actionProcs[command]()
+    return true
+
+proc addAction*(name, id: cstring, fn: proc()) =
+  var
+    commandId = pluginInfo.Register("command_id", unsafeAddr id)
+    accelReg: gaccel_register_t
+
+  accelReg.desc = name
+  accelReg.accel.cmd = commandId.uint16
+
+  discard pluginInfo.Register("gaccel", addr accelReg)
+  actionProcs[commandId] = fn
+
+# proc createWindow*() =
+#   proc windowProc(hWnd: HWND, msg: UINT, wParam: WPARAM, lParam: LPARAM): INT_PTR {.stdcall.} =
+#     case msg:
+#     of WM_MOUSEMOVE:
+#       case LOWORD(wParam):
+#       of MK_LBUTTON:
+#         ShowConsoleMsg($GET_X_LPARAM(lParam))
+#       else:
+#         discard
+#     else:
+#       discard
+
+#   let testWindow = CreateDialog(hInstance, MAKEINTRESOURCE(100), nil, windowProc)
+#   if testWindow != nil:
+#     discard ShowWindow(testWindow, SW_SHOW)
+
+template createExtension*(initCode: untyped): untyped =
   {.link: "resource/resource.res".}
 
   {.emit: """/*INCLUDESECTION*/
   #define REAPERAPI_IMPLEMENT
   """.}
-
-  var
-    hInstance {.inject.}: HINSTANCE
-    pluginInfo {.inject.}: ptr reaper_plugin_info_t
-    actionProcs {.inject.} = initTable[int, proc()]()
-
-  proc hookCommand(command: cint, flag: cint): bool =
-    if command != 0 and actionProcs.contains(command):
-      actionProcs[command]()
-      return true
-
-  proc addAction(name, id: cstring, fn: proc()) {.inject.} =
-    var
-      commandId = pluginInfo.Register("command_id", unsafeAddr id)
-      accelReg: gaccel_register_t
-
-    accelReg.desc = name
-    accelReg.accel.cmd = commandId.uint16
-
-    discard pluginInfo.Register("gaccel", addr accelReg)
-    actionProcs[commandId] = fn
 
   proc REAPER_PLUGIN_ENTRYPOINT(hInst: HINSTANCE, rec: ptr reaper_plugin_info_t): cint {.codegenDecl: "REAPER_PLUGIN_DLL_EXPORT $# $#$#", exportc, dynlib.} =
     hInstance = hInst
