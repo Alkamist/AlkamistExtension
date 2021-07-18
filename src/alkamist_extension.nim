40c4e80303516385637dc6478b8e0f354db550ed
Corey Lehman <coreylehman@me.com>
2021-07-18 13:00:31 -0400

    Success with drawing a rectangle in a window.

--- src/alkamist_extension.nim
+++ src/alkamist_extension.nim
@@ -1,22 +1,36 @@
-import
-  alkamist_extension/reaper
+import alkamist_extension/reaper
 
 proc windowProc(hWnd: HWND, msg: UINT, wParam: WPARAM, lParam: LPARAM): LRESULT {.stdcall.} =
   case msg:
+
+  of WM_PAINT:
+    var
+      ps = PAINTSTRUCT()
+      ctx = hWnd.BeginPaint(addr ps)
+      pen = CreatePen(PS_SOLID, 5, RGB(16, 16, 16))
+    discard ctx.SelectObject(pen)
+    discard ctx.Rectangle(50, 50, 200, 200)
+    discard hWnd.EndPaint(addr ps)
+
   of WM_MOUSEMOVE:
     case LOWORD(wParam):
     of MK_LBUTTON:
-      ShowConsoleMsg($GET_X_LPARAM(lParam))
+      # ShowConsoleMsg($GET_X_LPARAM(lParam))
+      discard
     else:
       discard
+
   else:
     discard
 
-proc testActionFn() =
+# proc testActionFn() =
+#   let testWindow = CreateDialog(hInstance, MAKEINTRESOURCE(100), pluginInfo.hwnd_main, cast[DLGPROC](windowProc))
+#   if testWindow != nil:
+#     discard ShowWindow(testWindow, SW_SHOW)
+
+createExtension:
+  # addAction("Alkamist: Test Action", "ALKAMIST_TEST_ACTION", testActionFn)
+  # ShowConsoleMsg("Alkamist Extension initialized.\n")
   let testWindow = CreateDialog(hInstance, MAKEINTRESOURCE(100), pluginInfo.hwnd_main, cast[DLGPROC](windowProc))
   if testWindow != nil:
     discard ShowWindow(testWindow, SW_SHOW)
-
-createExtension:
-  addAction("Alkamist: Test Action", "ALKAMIST_TEST_ACTION", testActionFn)
-  ShowConsoleMsg("Alkamist Extension initialized.\n")
--- src/alkamist_extension/reaper/winapi.nim
+++ src/alkamist_extension/reaper/winapi.nim
@@ -1,58 +1,67 @@
 when defined windows:
   {.passL: "user32.lib".}
+  {.passL: "gdi32.lib".}
+  {.pragma: windowsHeader, importc, header: "<windows.h>".}
+  {.pragma: windowsXHeader, importc, header: "<windowsx.h>".}
+else:
+  {.pragma: windowsHeader, importc, header: "swell/swell.h".}
+  {.pragma: windowsXHeader, importc, header: "swell/swell.h".}
 
 when defined cpu64:
   type
-    INT_PTR* {.importc, header: "<windows.h>".} = int64
-    UINT_PTR* {.importc, header: "<windows.h>".} = int64
-    LONG_PTR* {.importc, header: "<windows.h>".} = int64
-    ULONG_PTR* {.importc, header: "<windows.h>".} = int64
+    INT_PTR* {.windowsHeader.} = int64
+    UINT_PTR* {.windowsHeader.} = int64
+    LONG_PTR* {.windowsHeader.} = int64
+    ULONG_PTR* {.windowsHeader.} = int64
 else:
   type
-    INT_PTR* {.importc, header: "<windows.h>".} = int32
-    UINT_PTR* {.importc, header: "<windows.h>".} = int32
-    LONG_PTR* {.importc, header: "<windows.h>".} = int32
-    ULONG_PTR* {.importc, header: "<windows.h>".} = int32
+    INT_PTR* {.windowsHeader.} = int32
+    UINT_PTR* {.windowsHeader.} = int32
+    LONG_PTR* {.windowsHeader.} = int32
+    ULONG_PTR* {.windowsHeader.} = int32
 
 type
-  BOOL* {.importc, header: "<windows.h>".} = int32
-  CHAR* {.importc, header: "<windows.h>".} = char
-  WORD* {.importc, header: "<windows.h>".} = uint16
-  INT* {.importc, header: "<windows.h>".} = int32
-  LONG* {.importc, header: "<windows.h>".} = int32
-  BYTE* {.importc, header: "<windows.h>".} = uint8
-  UCHAR* {.importc, header: "<windows.h>".} = uint8
-  USHORT* {.importc, header: "<windows.h>".} = uint16
-  WCHAR* {.importc, header: "<windows.h>".} = uint16
-  UINT* {.importc, header: "<windows.h>".} = int32
-  ULONG* {.importc, header: "<windows.h>".} = int32
-  DWORD* {.importc, header: "<windows.h>".} = int32
-  FLOAT* {.importc, header: "<windows.h>".} = float32
-  WPARAM* {.importc, header: "<windows.h>".} = UINT_PTR
-  LPARAM* {.importc, header: "<windows.h>".} = LONG_PTR
-  LRESULT* {.importc, header: "<windows.h>".} = LONG_PTR
-  LPSTR* {.importc, header: "<windows.h>".} = cstring
-  LPCSTR* {.importc, header: "<windows.h>".} = cstring
-  LPWSTR* {.importc, header: "<windows.h>".} = cstring
-  LPCWSTR* {.importc, header: "<windows.h>".} = cstring
-  LPCTSTR* {.importc, header: "<windows.h>".} = cstring
+  BOOL* {.windowsHeader.} = int32
+  CHAR* {.windowsHeader.} = char
+  WORD* {.windowsHeader.} = uint16
+  INT* {.windowsHeader.} = int32
+  LONG* {.windowsHeader.} = int32
+  BYTE* {.windowsHeader.} = uint8
+  UCHAR* {.windowsHeader.} = uint8
+  USHORT* {.windowsHeader.} = uint16
+  WCHAR* {.windowsHeader.} = uint16
+  UINT* {.windowsHeader.} = int32
+  ULONG* {.windowsHeader.} = int32
+  DWORD* {.windowsHeader.} = int32
+  FLOAT* {.windowsHeader.} = float32
+  WPARAM* {.windowsHeader.} = UINT_PTR
+  LPARAM* {.windowsHeader.} = LONG_PTR
+  LRESULT* {.windowsHeader.} = LONG_PTR
+  LPSTR* {.windowsHeader.} = cstring
+  LPCSTR* {.windowsHeader.} = cstring
+  LPWSTR* {.windowsHeader.} = cstring
+  LPCWSTR* {.windowsHeader.} = cstring
+  LPCTSTR* {.windowsHeader.} = cstring
 
-  HANDLE* {.importc, header: "<windows.h>".} = pointer
-  HINSTANCE* {.importc, header: "<windows.h>".} = HANDLE
-  HWND* {.importc, header: "<windows.h>".} = HANDLE
-  HMENU* {.importc, header: "<windows.h>".} = HANDLE
+  HANDLE* {.windowsHeader.} = pointer
+  HINSTANCE* {.windowsHeader.} = HANDLE
+  HWND* {.windowsHeader.} = HANDLE
+  HMENU* {.windowsHeader.} = HANDLE
+  HDC* {.windowsHeader.} = HANDLE
+  HPEN* {.windowsHeader.} = HANDLE
+  HGDIOBJ* {.windowsHeader.} = HANDLE
 
-  POINT* {.importc, header: "<windows.h>".} = object
+  POINT* {.windowsHeader.} = object
     x*: LONG
     y*: LONG
 
-  RECT* {.importc, header: "<windows.h>".} = object
+  RECT* {.windowsHeader.} = object
     left*: LONG
     top*: LONG
     right*: LONG
     bottom*: LONG
 
-  MSG* {.importc, header: "<windows.h>".} = object
+  MSG* {.windowsHeader.} = object
     hwnd*: HWND
     message*: UINT
     wParam*: WPARAM
@@ -60,19 +69,30 @@ type
     time*: DWORD
     pt*: POINT
 
-  GUID* {.importc, header: "<windows.h>".} = object
+  GUID* {.windowsHeader.} = object
     Data1*: int32
     Data2*: uint16
     Data3*: uint16
     Data4*: array[8, uint8]
 
-  ACCEL* {.importc, header: "<windows.h>".} = object
+  ACCEL* {.windowsHeader.} = object
     fVirt*: BYTE
     key*: WORD
     cmd*: WORD
 
-  WNDPROC* {.importc, header: "<windows.h>".} = proc (hWnd: HWND, msg: UINT, wParam: WPARAM, lParam: LPARAM): LRESULT {.stdcall.}
-  DLGPROC* {.importc, header: "<windows.h>".} = proc(hWnd: HWND, msg: UINT, wParam: WPARAM, lParam: LPARAM): INT_PTR {.stdcall.}
+  PAINTSTRUCT* {.windowsHeader.} = object
+    hdc*: HDC
+    fErase*: BOOL
+    rcPaint*: RECT
+    fRestore*: BOOL
+    fIncUpdate*: BOOL
+    rgbReserved*: array[32, BYTE]
+  LPPAINTSTRUCT* = ptr PAINTSTRUCT
+
+  WNDPROC* {.windowsHeader.} = proc (hWnd: HWND, msg: UINT, wParam: WPARAM, lParam: LPARAM): LRESULT {.stdcall.}
+  DLGPROC* {.windowsHeader.} = proc(hWnd: HWND, msg: UINT, wParam: WPARAM, lParam: LPARAM): INT_PTR {.stdcall.}
+
+  COLORREF* {.windowsHeader.} = DWORD
 
 const
   WM_KEYDOWN* = 0x0100
@@ -80,6 +100,7 @@ const
   WM_SYSKEYDOWN* = 0x0104
   WM_SYSKEYUP* = 0x0105
   WM_MOUSEMOVE* = 0x0200
+  WM_PAINT* = 0x000f
   MK_CONTROL* = 0x0008
   MK_LBUTTON* = 0x0001
   MK_MBUTTON* = 0x0010
@@ -101,12 +122,36 @@ const
   SW_RESTORE* = 9
   SW_SHOWDEFAULT* = 10
   SW_FORCEMINIMIZE* = 11
+  PS_COSMETIC* = 0x00000000
+  PS_ENDCAP_ROUND* = 0x00000000
+  PS_JOIN_ROUND* = 0x00000000
+  PS_SOLID* = 0x00000000
+  PS_DASH* = 0x00000001
+  PS_DOT* = 0x00000002
+  PS_DASHDOT* = 0x00000003
+  PS_DASHDOTDOT* = 0x00000004
+  PS_NULL* = 0x00000005
+  PS_INSIDEFRAME* = 0x00000006
+  PS_USERSTYLE* = 0x00000007
+  PS_ALTERNATE* = 0x00000008
+  PS_ENDCAP_SQUARE* = 0x00000100
+  PS_ENDCAP_FLAT* = 0x00000200
+  PS_JOIN_BEVEL* = 0x00001000
+  PS_JOIN_MITER* = 0x00002000
+  PS_GEOMETRIC* = 0x00010000
 
-proc LOWORD*(value: WPARAM): int {.stdcall, importc, header: "<windowsx.h>".}
-proc HIWORD*(value: WPARAM): int {.stdcall, importc, header: "<windowsx.h>".}
-proc GET_X_LPARAM*(lParam: LPARAM): int {.stdcall, importc, header: "<windowsx.h>".}
-proc GET_Y_LPARAM*(lParam: LPARAM): int {.stdcall, importc, header: "<windowsx.h>".}
-proc MAKEINTRESOURCE*(i: int): LPCTSTR {.stdcall, importc, header: "<windows.h>".}
+proc LOWORD*(value: WPARAM): int {.stdcall, windowsXHeader.}
+proc HIWORD*(value: WPARAM): int {.stdcall, windowsXHeader.}
+proc GET_X_LPARAM*(lParam: LPARAM): int {.stdcall, windowsXHeader.}
+proc GET_Y_LPARAM*(lParam: LPARAM): int {.stdcall, windowsXHeader.}
+proc MAKEINTRESOURCE*(i: int): LPCTSTR {.stdcall, windowsHeader.}
 
-proc CreateDialog*(hInstance: HINSTANCE, lpName: LPCTSTR, hWndParent: HWND, lpDialogFunc: DLGPROC): HWND {.stdcall, importc, header: "<windows.h>".}
-proc ShowWindow*(hWnd: HINSTANCE, nCmdShow: int32): BOOL {.stdcall, importc, header: "<windows.h>".}
+proc CreateDialog*(hInstance: HINSTANCE, lpName: LPCTSTR, hWndParent: HWND, lpDialogFunc: DLGPROC): HWND {.stdcall, windowsHeader.}
+proc ShowWindow*(hWnd: HINSTANCE, nCmdShow: int32): BOOL {.stdcall, windowsHeader.}
+
+proc BeginPaint*(hWnd: HWND, lpPaint: LPPAINTSTRUCT): HDC {.stdcall, windowsHeader.}
+proc EndPaint*(hWnd: HWND, lpPaint: ptr PAINTSTRUCT): BOOL {.stdcall, windowsHeader.}
+proc RGB*(r, g, b: int): COLORREF {.stdcall, windowsHeader.}
+proc CreatePen*(iStyle, cWidth: int, color: COLORREF): HPEN {.stdcall, windowsHeader.}
+proc SelectObject*(hdc: HDC, h: HGDIOBJ): HGDIOBJ {.stdcall, windowsHeader.}
+proc Rectangle*(ctx: HDC; l, t, r, b: int): BOOL {.stdcall, windowsHeader.}
